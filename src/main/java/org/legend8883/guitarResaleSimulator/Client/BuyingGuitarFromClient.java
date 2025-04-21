b0163f37eb81cd7de3316de39f98f4389ea92673
Legend8883 <minecraftspeed3@gmail.com>
2025-04-21 20:11:40 +0300

    Added random type and price of guitar in BuyingGuitarFromClient from properties file

--- src/main/java/org/legend8883/guitarResaleSimulator/Client/BuyingGuitarFromClient.java
+++ src/main/java/org/legend8883/guitarResaleSimulator/Client/BuyingGuitarFromClient.java
@@ -12,21 +12,57 @@ public class BuyingGuitarFromClient {
     private Random random = new Random();
 
     @Value("${client.names}")
-    private String names;
+    private String clientNames;
+    @Value("${guitar.names}")
+    private String guitarName;
 
     public void buy() {
 
         int randomValue;
 
+        //Генерация случайного имени с файла
         ArrayList<String> namesList = new ArrayList<>();
-        for (String name : names.split(", ")) {
+
+        for (String name : clientNames.split(", ")) {
             namesList.add(name);
         }
 
-        randomValue = random.nextInt(0, 5);
+        randomValue = random.nextInt(namesList.size());
 
-        String name = namesList.get(randomValue);
+        String clientName;
+        clientName = namesList.get(randomValue);
 
-        System.out.println("Покупка гитары у " + name);
+        System.out.println("Покупка гитары у " + clientName);
+
+
+        //Генерация случайной цены относительно той, которая указана в файле
+        ArrayList<String> guitarsList = new ArrayList<>();
+
+        for (String guitarName : guitarName.split(", ")) {
+            guitarsList.add(guitarName);
+        }
+
+        randomValue = random.nextInt(guitarsList.size());
+
+        String guitarTemp = guitarsList.get(randomValue);
+
+        guitarsList.clear();
+
+        for (String guitar : guitarTemp.split("=")) {
+            guitarsList.add(guitar);
+        }
+        String guitarName = guitarsList.get(0);
+        try {
+            double guitarPrice = Double.parseDouble(guitarsList.get(1));
+
+            System.out.println(
+                    "Тип гитары: " + guitarName + "\n" +
+                    "Цена: " + guitarPrice
+            );
+        } catch (NumberFormatException e) {
+            System.out.println("Введите double значение для гитары " + guitarName + " в файле names.properties");
+        }
+
+
     }
 }
